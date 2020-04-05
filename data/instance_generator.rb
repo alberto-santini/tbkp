3bda3e58ca9a014dbb4defd95fe8052227946042
Alberto Santini <santini.alberto@gmail.com>
2020-04-05 18:15:08 +0200

    Added uv instances

--- data/instance_generator.rb
+++ data/instance_generator.rb
@@ -1,15 +1,29 @@
-require 'statistics/distribution/normal'
+require 'simple-random'
 
 def generate_uu(n, k, c, b)
     w = (k * c / n).floor.to_i
     weights = Array.new(n) {w}
 
-    profit_noise = Statistics::Distribution::Normal.new(0, w/8.0)
-    profits = Array.new(n) { [1, (w + profit_noise.random).ceil].max }.map(&:to_i)
+    r = SimpleRandom.new
+    profits = Array.new(n) {[1, (w + r.normal(0, w/8.0)).ceil].max}.map(&:to_i)
 
     pprod = 0.5 * profits.min
     probabilities = Array.new(n) do |i|
         (rand() < b) ? pprod / profits[i] : 1
+    end.map{|pi| pi.round(6)}
+
+    return weights, profits, probabilities
+end
+
+def generate_uv(n, k, c, b)
+    w = (k * c / n).floor.to_i
+    weights = Array.new(n) {w}
+
+    r = SimpleRandom.new
+    profits = Array.new(n) {[1, (w + r.normal(0, w/8.0)).ceil].max}.map(&:to_i)
+
+    probabilities = Array.new(n) do
+        (rand() < b) ? (1.0 - r.beta(2, 10)) : 1
     end.map{|pi| pi.round(6)}
 
     return weights, profits, probabilities
@@ -43,4 +57,24 @@ def generate_all_uu
     end
 end
 
-generate_all_uu
+def generate_all_uv
+    c = 100000
+
+    [100, 500, 1000, 5000].each do |n|
+        [1.6, 1.8, 2.0, 2.2, 2.4].each do |k|
+            bs = [0.05, 0.1, 0.2, 0.5]
+            bs << 0.01 if n >= 1000
+
+            bs.each do |b|
+                1.upto(5) do |inst_n|
+                    w, p, pi = generate_uv(n, k, c, b)
+                    filename = "uv-#{n}-#{k}-#{b}-#{inst_n}.txt"
+                    filename = File.join('generated-instances', filename)
+                    print_instance(filename, n, c, w, p, pi)
+                end
+            end
+        end
+    end
+end
+
+generate_all_uv
