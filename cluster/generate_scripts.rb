043422bb867c2fb929f3b890076f3b22ce7088cc
Alberto Santini <santini.alberto@gmail.com>
2020-04-06 12:58:29 +0200

    Added cluster script creator

---
+++ cluster/generate_scripts.rb
@@ -0,0 +1,57 @@
+require 'fileutils'
+
+L = "/homes/users/asantini/local/lib:/homes/users/asantini/local/lib64"
+G = "/homes/users/asantini/local/etc/gurobi.lic"
+E = "/homes/users/asantini/local/src/tbkp/build/tbkp"
+S = <<~EOF
+    #!/bin/bash
+    #SBATCH --partition=normal
+    #SBATCH --time=02:00:00
+    #SBATCH --nodes=1
+    #SBATCH --ntasks-per-node=1
+    #SBATCH --cpus-per-task=1
+    #SBATCH --mem-per-cpu=4GB
+    #SBATCH -o /dev/null
+EOF
+
+def create_script(instance, use_de, use_boole)
+    instance = File.join(
+        '/homes/users/asantini/local/src/tbkp/data/generated-instances',
+        File.basename(instance)
+    )
+
+    b = File.basename(instance, '.txt')
+    b += "-d" if use_de
+    b += "-b" if use_boole
+
+    script_f = File.join('scripts', "launch-#{b}.sh")
+    error_f = File.join('/homes/users/asantini/local/src/tbkp/cluster/scripts', "err-#{b}.txt")
+    output_f = File.join('/homes/users/asantini/local/src/tbkp/cluster/output', "res-#{b}.txt")
+    
+    params = ""
+    params += " -d" if use_de
+    params += " -b" if use_boole
+
+    script = <<~EOF
+        #{S.strip}
+        #SBATCH -e #{error_f}
+
+        LD_LIBRARY_PATH=#{L} GRB_LICENSE_FILE=#{G} #{E} -i #{instance} -o #{output_f} -t 3600#{params}
+    EOF
+
+    File.write(script_f, script)
+end
+
+def create_all_scripts
+    Dir.glob('../data/generated-instances/*.txt') do |instance|
+        [true, false].each do |use_de|
+            [true, false].each do |use_boole|
+                create_script(instance, use_de, use_boole)
+            end
+        end
+    end
+end
+
+FileUtils.mkdir_p('scripts')
+FileUtils.mkdir_p('output')
+create_all_scripts
